c7186d5d42c801606b6a6f3c7a95c2cef1962207
mcrucq <mcrucq@gmail.com>
2022-08-25 21:42:55 +0200

    fin j1
    fin j1

---
+++ scriptBS-WA.js
@@ -0,0 +1,67 @@
+// Chargement de la lib JS de WA
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+
+//Prompt des zones
+let escaliers;
+let welcome;
+
+WA.room.onEnterLayer("_prompt_escaliers").subscribe(() => {
+    escaliers = WA.ui.openPopup("promptEscaliers", "Désolé, le 1er étage n'est pas encore accessible :-(", [{
+        label: "Tant pis",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("_prompt_escaliers").subscribe(() => {
+    escaliers.close();
+})
+
+WA.room.onEnterLayer("_prompt_welcome").subscribe(() => {
+    welcome = WA.ui.openPopup("promptWelcome", "test", [{
+        label: "Tant pis",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("_prompt_welcome").subscribe(() => {
+    welcome.close();
+})
+
+
+
+class Popup {
+
+    close() {};
+}
+
+/*let escaliers;
+
+WA.room.onEnterLayer("_prompt_escaliers").subscribe(() => {
+    escaliers = WA.ui.openPopup("promptEscaliers", "Désolé, le 1er étage n'est pas encore accessible :-(", [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("myZone").subscribe(() => {
+    escaliers.close();
+})
+
+class Popup {
+    close() {};
+} */
