6600da4ea6c94232c1dc689466dd30cb3f5a53a7
mcrucq <mcrucq@gmail.com>
2022-09-05 09:07:22 +0200

    update lundi
    update lundi

--- scriptBS-WA.js
+++ scriptBS-WA.js
@@ -7,7 +7,10 @@ let welcome;
 let msgGuillaume;
 let userName;
 let msgFlavien;
+let msgBrice;
+let msgCyril;
 
+///////////////////////////////////////////////
 
 WA.room.onEnterLayer("_website_perso_guillaumeM_tribune").subscribe(() => {
     msgGuillaume = WA.ui.openPopup("messageGuillaume", "Guillaume Mikowski : 'Norme ISO 20121, on nous prend vraiment pour des cons ! Je vous en parle sur Linkedin.''", [{
@@ -25,6 +28,44 @@ WA.room.onLeaveLayer("_website_perso_guillaumeM_tribune").subscribe(() => {
     msgGuillaume.close();
 })
 
+///////////////////////////////////////////////
+
+WA.room.onEnterLayer("_website_heroes").subscribe(() => {
+    msgBrice = WA.ui.openPopup("LabMessage", "Bienvenue dans le lab Heroes ! Ici, on fabrique les talents pour renforcer les équipes de nos clients. ''", [{
+        label: "Génial, je veux en savoir plus !",
+        className: "primary",
+        callback: (popup) => {
+            WA.nav.openTab("https://heroes.brainsonic.com/");
+        }
+    }]);
+
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("_website_heroes").subscribe(() => {
+    msgBrice.close();
+})
+
+///////////////////////////////////////////////
+
+WA.room.onEnterLayer("_website_cyril").subscribe(() => {
+    msgCyril = WA.ui.openPopup("msgCyril", "Salut, c'est Cyril. Ici, je vous propose une sélection des meilleurs livres consacrés au marketing. Revenez régulièrement, on met à jour toutes les semaines !", [{
+        label: "Carrément !",
+        className: "primary",
+        callback: (popup) => {
+            WA.nav.openTab("https://cdhenin.notion.site/Livres-La-s-lection-du-p-le-Editorial-0b58151e539a4895936e62d3aabc2b7d");
+        }
+    }]);
+
+});
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("_website_cyril").subscribe(() => {
+    msgCyril.close();
+})
+
+///////////////////////////////////////////////
+
 WA.room.onEnterLayer("_website_perso_flav").subscribe(() => {
     msgFlavien = WA.ui.openPopup("messageFlavien", "Flavien Lefort : Evénement digital, ce que 20 ans (ou presque) nous ont appris - à lire sur Linkedin", [{
         label: "Je veux la lire",
@@ -39,6 +80,8 @@ WA.room.onEnterLayer("_website_perso_flav").subscribe(() => {
 WA.room.onLeaveLayer("_website_perso_flav").subscribe(() => {
     msgFlavien.close();
 })
+
+///////////////////////////////////////////////
 
 WA.room.onEnterLayer("_prompt_escaliers").subscribe(() => {
     escaliers = WA.ui.openPopup("promptEscaliers", "Toutes nos excuses, le 1er étage est en travaux ! Revenez un peu plus tard.", [{
@@ -55,6 +98,8 @@ WA.room.onEnterLayer("_prompt_escaliers").subscribe(() => {
 WA.room.onLeaveLayer("_prompt_escaliers").subscribe(() => {
     escaliers.close();
 })
+
+///////////////////////////////////////////////
 
 WA.onInit().then(() => {
     userName = WA.player.name;
@@ -78,10 +123,7 @@ WA.room.onLeaveLayer("_prompt_welcome").subscribe(() => {
     welcome.close();
 })
 
-
-//WA.chat.sendChatMessage('Bonjour, et bienvenue chez Brainsonic', 'Garance');
-//WA.chat.sendChatMessage('test 2', 'Garance');
-
+///////////////////////////////////////////////
 
 
 
